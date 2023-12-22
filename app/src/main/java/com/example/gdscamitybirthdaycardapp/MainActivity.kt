a84e6ca9c57c3f67e7d87f4a6513223968c20940
atyagiabhi210 <95032729+atyagiabhi210@users.noreply.github.com>
2023-12-22 23:42:16 +0530

    Final Project

--- app/src/main/java/com/example/gdscamitybirthdaycardapp/MainActivity.kt
+++ app/src/main/java/com/example/gdscamitybirthdaycardapp/MainActivity.kt
@@ -1,46 +1,60 @@
 package com.example.gdscamitybirthdaycardapp
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.gdscamitybirthdaycardapp.ui.theme.GdscAmityBirthdayCardAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GdscAmityBirthdayCardAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            val painter= painterResource(id = R.drawable.cake)
+            BirthdayGreeting(painter = painter, salutation ="Happy Birthday" , name ="Abhi" , contentDescription ="An image of the cake" )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BirthdayGreeting(
+    painter: Painter,
+    salutation:String,
+    name:String,
+    modifier: Modifier=Modifier,
+    contentDescription:String
+    ){
+    Column(modifier= Modifier
+        .fillMaxHeight()
+        .fillMaxWidth(),
+        Arrangement.Center) {
+
+        Text(text = salutation, style = TextStyle(fontSize = 30.sp))
+        Text(text = name)
+        Image(painter = painter, contentDescription = contentDescription)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GdscAmityBirthdayCardAppTheme {
-        Greeting("Android")
+       // Greeting("Android")
     }
 }
